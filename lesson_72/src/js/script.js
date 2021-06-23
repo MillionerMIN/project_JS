3bc70216210f9c5a4918412dccf7495fdea242b2
Vladimir <vladimirlyn@gmail.com>
2021-06-23 10:29:19 +0300

    lesson_72 useing ES6 Modules

---
+++ lesson_72/src/js/script.js
@@ -0,0 +1,13 @@
+import {one, two} from './main.js';
+// import {one as first} from './main';
+// import * as data from './main';
+import sayHi from './main.js';
+
+
+// console.log(first);
+
+console.log(`${one} and ${two}`);
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
+
+sayHi();
